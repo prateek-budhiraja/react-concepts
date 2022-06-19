d7e6b7f9077ab652e044ed9d7194d5ad9243ee75
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-06-19 17:18:24 +0530

    about page updated

--- capstone-product-cart/src/components/static-pages/About.js
+++ capstone-product-cart/src/components/static-pages/About.js
@@ -1,5 +1,19 @@
+import "./styles.css";
+
 const About = () => {
-	return <h1>About Page</h1>;
+	return (
+		<div className="about">
+			<h1 className="mt-5">About</h1>
+			<p class="col-lg-6 col-md-8">
+				📥 Super Store is an inventory management system using React. It uses
+				Json Server as a backend. To make a new account, navigate to Sign Up
+				page and if you already have an account go to Sign In page. You can view
+				the products even without login but to add, update, delete the items you
+				have to Sign In. You can also navigate to your profile page after you
+				Log In.
+			</p>
+		</div>
+	);
 };
 
 export default About;
