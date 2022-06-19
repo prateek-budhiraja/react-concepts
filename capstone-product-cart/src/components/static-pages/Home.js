29026be8f9d2c390b2b165be85a971e4643bb5a7
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2022-06-19 16:47:39 +0530

    added home page

--- capstone-product-cart/src/components/static-pages/Home.js
+++ capstone-product-cart/src/components/static-pages/Home.js
@@ -1,5 +1,38 @@
+import { Row, Col, Container, Button } from "reactstrap";
+import IndividualNavLink from "../nav/IndividualNavLink";
+
+import "./styles.css";
+
 const Home = () => {
-	return <h1>Home Page</h1>;
+	return (
+		// <Container className="home">
+		<Row className="w-100">
+			<Col className="home-content p-0 col-lg-6 col-md-6 col-12 mx-0 text-center">
+				<h1 className="text-primary w-100">📥 Super Store</h1>
+				<pre className="w-100 text-muted">
+					{`Inventory management software 
+designed for Indian businesses`}
+				</pre>
+				<IndividualNavLink
+					isButton={true}
+					label={"SIGN UP"}
+					route={"/signup"}
+				/>
+				<IndividualNavLink
+					isButton={true}
+					label={"SIGN IN"}
+					route={"/signin"}
+				/>
+			</Col>
+			<Col className="col-lg-6 col-md-6 col-0">
+				<img
+					src={process.env.PUBLIC_URL + "/static/images/landing.jpeg"}
+					className="col-lg-12 col-md-12 col-0"
+				/>
+			</Col>
+		</Row>
+		// </Container>
+	);
 };
 
 export default Home;
